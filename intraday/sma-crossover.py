201aea7bb68b1c1729759f902020fb502bf73738
Gireesh <36097656+GireeshS22@users.noreply.github.com>
2018-03-20 00:10:44 +0530

    Multiple updates for AWS
     updated IST to UTC
    updated sleep to inside waiting block in else
    updated list of scripts
    updated code segment with input sttement

--- intraday/sma-crossover.py
+++ intraday/sma-crossover.py
@@ -11,17 +11,16 @@ from datetime import datetime, time
 import time as sleep
 import os
 import pandas as pd
-from matplotlib import pyplot as plt
 
 #%%
-api_key = "<YOUR API KEY>"
-api_secret = "<YOUR API SECRET>"
-redirect_uri = "<YOUR REDIRECT URI>"
+api_key = "your_api_key"
+api_secret = "api_secret"
+redirect_uri = "http://127.0.0.1"
 s = Session(api_key)
 s.set_redirect_uri(redirect_uri)
 s.set_api_secret(api_secret)
 print(s.get_login_url())
-code = "<CODE THAT YOU GET FROM URL>"
+code = input("Enter the code from the browser: ")
 
 s.set_code (code)
 access_token = s.retrieve_access_token()
@@ -81,41 +80,40 @@ def sell(script, amount, stoploss, squareoff):
 
 def SMACrossOver(ScriptData):
     if ScriptData.sma5.iloc[-6] < ScriptData.sma50.iloc[-6] and ScriptData.sma5.iloc[-5] < ScriptData.sma50.iloc[-5] and ScriptData.sma5.iloc[-4] < ScriptData.sma50.iloc[-4] and ScriptData.sma5.iloc[-3] < ScriptData.sma50.iloc[-3] and ScriptData.sma5.iloc[-2] > ScriptData.sma50.iloc[-2] and ScriptData.sma5.iloc[-1] > ScriptData.sma50.iloc[-1]:
-        squareoff = float(round(abs(ScriptData.cp.iloc[-1] - ScriptData.high.iloc[-1] * 1.01), 0))
-        stoploss = float(round(abs(ScriptData.cp.iloc[-1] - ScriptData.low.iloc[-1] * 0.99), 0))
+        squareoff = float(round(abs(ScriptData.cp.iloc[-1] - (ScriptData.cp.iloc[-1] * 1.01)), 0))
+        stoploss = float(round(abs(ScriptData.cp.iloc[-1] - (ScriptData.cp.iloc[-1] * 1.01)), 0))
         print("Buying at: %s -- stop loss at: %s --  square off at: %s" %(ScriptData.cp.iloc[-1], stoploss, squareoff))
         buy(script, ScriptData.cp.iloc[-1], stoploss, squareoff)
         
     if ScriptData.sma5.iloc[-6] > ScriptData.sma50.iloc[-6] and ScriptData.sma5.iloc[-5] > ScriptData.sma50.iloc[-5] and ScriptData.sma5.iloc[-4] > ScriptData.sma50.iloc[-4] and ScriptData.sma5.iloc[-3] > ScriptData.sma50.iloc[-3] and ScriptData.sma5.iloc[-2] < ScriptData.sma50.iloc[-2] and ScriptData.sma5.iloc[-1] < ScriptData.sma50.iloc[-1]:
-        stoploss = float(round(abs(ScriptData.cp.iloc[-1] - ScriptData.high.iloc[-1] * 1.01), 0))
-        squareoff = float(round(abs(ScriptData.cp.iloc[-1] - ScriptData.low.iloc[-1] * 0.99), 0))
+        squareoff = float(round(abs(ScriptData.cp.iloc[-1] - (ScriptData.cp.iloc[-1] * 1.01)), 0))
+        stoploss = float(round(abs(ScriptData.cp.iloc[-1] - (ScriptData.cp.iloc[-1] * 1.01)), 0))
         print("Selling at: %s -- stop loss at: %s --  square off at: %s" %(ScriptData.cp.iloc[-1], stoploss, squareoff))
         sell(script, ScriptData.cp.iloc[-1], stoploss, squareoff)
         
         
 #%%
-def CheckTrades():
+def CheckTrades():    
     now = datetime.now()
     now_time = now.time()
-    
-    print("Now checking trades at: %s" %s)
-    
-    if time(10,11) <= now_time <= time(14,00) and CheckBalance() > 1500:
-        bucket = ["APOLLOTYRE","AXISBANK","BHARTIARTL", "INDIACEM", "JETAIRWAYS",
-                    "JINDALSTEL","L&TFH", "RELIANCE","SBIN", "TATAMOTORS"]
+
+    if time(05,41) <= now_time <= time(08,45) and CheckBalance() > 1500:
+        bucket = ["BANKINDIA", "ADANIENT", "CANBK", "JINDALSTEL", "VAKRANGEE", "FORTIS", "JISLJALEQS", "GRAPHITE", "PCJEWELLER", "BOMDYEING", "RELCAPITAL", "IBREALEST", "INFIBEAM", "KTKBANK", "TATAGLOBAL"]
         
         for script in bucket:
-            print("~~~~~~~~~~~~~~~~~~~~~~~ \n Now the time is: %s" %now_time)
+            print("~~~~~~~~~~~~~~~~~~~~~~~ \n Now the time is: %s" % datetime.now().time())
             print("Checking for 50 min 5min MA Crossover for %s" % script)
             SMACrossOver(historicData(script, "19/03/2018", "19/03/2018"))
             
-    elif now_time >= time(14,55):
+    elif time(09,28) <= now_time <= time(09,30):
         print("Exiting all the open position now and exiting execution")
         u.cancel_all_orders() #Cancel all open orders
         exit()
-
+        
+    else:
+        print("There is no market activity now. Checking in 10 mins.. Now the time is: %s" % datetime.now().time())
+        sleep.sleep(120)
+        
 #%%
 while True:
     CheckTrades()
-    sleep.sleep(630)
-#%%
